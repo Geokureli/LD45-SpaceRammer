2b55ad768a08b964ea99859c924ba5a62179ff5e
George FunBook <gkurelic@gmail.com>
2019-10-24 16:27:23 -0500

    error repro changes/tests

--- source/states/ErrorReproState.hx
+++ source/states/ErrorReproState.hx
@@ -6,14 +6,16 @@ class ErrorReproState extends flixel.FlxState
     {
         super.create();
         
-        createSprite(100, 100, 15);              // shows
-        createSprite(200, 100);                  // doesn't show
-        createSprite(300, 100);                  // doesn't show
-        createSprite(400, 100).color = 0xffff00; // shows
+        add(createSprite(100, 100, 15));              // shows
+        add(createSprite(200, 100,  9));              // doesn't show
+        add(createSprite(300, 100,  9));              // doesn't show
+        add(createSprite(400, 100,  9, 0xffff00)); // shows
     }
     
-    inline function createSprite(x = 0.0, y = 0.0, size = 9):flixel.FlxSprite
-    { 
-        return cast add(new flixel.FlxSprite(x, y).makeGraphic(size, size));
+    inline function createSprite(x = 0.0, y = 0.0, size:Int, color = 0xffffff):flixel.FlxSprite
+    {
+        var sprite = new flixel.FlxSprite(x, y).makeGraphic(size, size);
+        sprite.color = color;
+        return sprite;
     }
 }
